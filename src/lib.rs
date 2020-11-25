66e4b16ded9929e0604fd7e76657aba304bc24d6
Martin R <23411718+RonquilloAeon@users.noreply.github.com>
2020-11-24 19:45:06 -0600

    Add UUID4 generation capability

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+use std::env;
+use std::error::Error;
+
+use uuid::Uuid;
+
 pub struct Config {
-    version: String
+    count: u8,
+}
+
+impl Config {
+    pub fn count(&self) -> &u8 {
+        &self.count
+    }
+}
+
+impl Config {
+    pub fn new(mut args: env::Args) -> Result<Self, Box<dyn Error>> {
+        args.next();
+
+        let count = match args.next() {
+            Some(arg) => arg.parse::<u8>()?,
+            None => 1,
+        };
+
+        Ok(Config { count })
+    }
+}
+
+pub fn generate_uuid(config: &Config) -> Result<Vec<String>, Box<dyn Error>> {
+    let mut uuids: Vec<String> = vec![];
+
+    for _ in 0..config.count {
+        uuids.push(Uuid::new_v4().to_hyphenated().to_string())
+    }
+
+    Ok(uuids)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,24 @@ use std::process;
 use uuidme::Config;
 
 fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Error in arguments: {}", err);
+        process::exit(1);
+    });
+
+    let uuids = uuidme::generate_uuid(&config).unwrap_or_else(|err| {
+        eprintln!("Unable to generate a new UUID: {}", err);
+        process::exit(1);
+    });
+
+    match config.count() {
+        1 => println!("Generated 1 UUID"),
+        count => println!("Generated {} UUIDs:", count),
+    }
+
+    for uuid in uuids.iter() {
+        println!("{}", uuid);
+    }
+
     process::exit(0);
 }
